2b48821daaffee23b0d5633cc8ac13728016da25
손병화 <sonbyeonghwa@sonbyeonghwaui-MacBook-Pro.local>
2017-08-30 17:08:42 +0900

    view selector

--- src/components/ViewSelector.js
+++ src/components/ViewSelector.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import oc from 'open-color';
 import StarIcon from 'react-icons/lib/md/star';
 import PeopleIcon from 'react-icons/lib/md/people';
+import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
     height: 4rem;
@@ -23,7 +24,7 @@ const StyledItem = styled.div`
     align-items: center;
     justify-content: center;
     
-    color: ${oc.gray[6]};
+    color: ${props => props.active ? oc.gray[9] : oc.gray[6]};
     
     font-size: 1.5rem;
     cursor: pointer;
@@ -33,6 +34,10 @@ const StyledItem = styled.div`
     }
 `;
 
+StyledItem.propTypes = {
+    active: PropTypes.bool
+};
+
 const Bar = styled.div`
     position: absolute;
     bottom: 0px;
@@ -40,20 +45,47 @@ const Bar = styled.div`
     
     width: 50%;
     background: ${oc.cyan[5]};
+    
+    transition: ease-in .25s;
+    
+    transform: ${props => props.right ? 'translateX(100%)' : 'none'};
 `;
 
-const Item = ({children}) => (
-    <StyledItem>
+Bar.propTypes = {
+    right: PropTypes.bool
+};
+
+const Item = ({children, selected, name, onSelect}) => (
+    <StyledItem onClick={() => onSelect(name)} active={selected === name}>
         {children}
     </StyledItem>
 );
 
-const ViewSelector = () => (
+Item.propTypes = {
+    selected: PropTypes.string,
+    name: PropTypes.string,
+    onSelect: PropTypes.func
+};
+
+const ViewSelector = ({selected, onSelect}) => (
     <Wrapper>
-        <Item><StarIcon/></Item>
-        <Item><PeopleIcon/></Item>
-        <Bar/>
+        <Item
+            selected={selected}
+            name="favorite"
+            onSelect={onSelect}
+        ><StarIcon/></Item>
+        <Item
+            selected={selected}
+            name="list"
+            onSelect={onSelect}
+        ><PeopleIcon/></Item>
+        <Bar right={selected === 'list'}/>
     </Wrapper>
 );
+
+ViewSelector.propTypes = {
+    selected: PropTypes.string,
+    onSelect: PropTypes.func
+};
 
 export default ViewSelector;
