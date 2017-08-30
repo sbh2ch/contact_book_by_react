23b96ad89172e81fe02b3475a612c3d0b210d541
손병화 <sonbyeonghwa@sonbyeonghwaui-MacBook-Pro.local>
2017-08-30 14:35:54 +0900

    update

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Container from './components/Container';
 import ViewSelector from './components/ViewSelector';
 
 class App extends Component {
+    state = {
+        view: 'favorite'
+    };
+
+    handleSelectView = (view) => this.setState({view});
+
     render() {
+        const {view} = this.state;
+        const {handleSelectView} = this;
+
         return (
-            <div className="App">
+            <div>
                 <Header/>
-                <ViewSelector/>
-                <Container></Container>
+                <ViewSelector
+                    onSelect={handleSelectView} selected={view}
+                />
+                <Container visible={view === 'favorite'}>favorite</Container>
+                <Container visible={view === 'list'}>list</Container>
             </div>
         );
-    }
+    };
 }
 
 export default App;
